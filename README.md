1804c194456487305545d421a521e1d35f22a9cc
François Mockers <mockersf@gmail.com>
2018-04-14 23:56:01 +0200

    example avec serde

--- README.md
+++ README.md
@@ -3,10 +3,20 @@
 * Utilisation de serde
   * Dérivation des traits `Serialize` / `Deserialize`
   * Les attributs de serde
-  * Écrire du json dans une String avec `serde_json::to_string`
+  * Écrire du json avec `serde_json::to_*`
   * Lire le json avec `serde_json::from_*`
+
+```
+curl http://localhost:7878/hello -X POST -d '{"say hello to": "Rust"}'
+curl http://localhost:7878/goodbye/Devoxx
+```
 
 * À faire:
   * Créer les structs pour la réponse avec les ratings
   * Implémenter le trait `IntoResponse` de Gotham
   * Créer la struct pour créer un rating
+
+```
+curl http://localhost/ratings/5
+curl http://localhost/ratings/5 -X POST -d '{"reviewer": "my_name", "rating": 3}'
+```
--- ratings/src/main.rs
+++ ratings/src/main.rs
@@ -9,86 +9,96 @@ extern crate mime;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 
 use gotham::http::response::create_response;
 use gotham::state::{FromState, State};
 use gotham::router::Router;
 use gotham::router::builder::*;
-use gotham::handler::{HandlerFuture, IntoHandlerError};
+use gotham::handler::{HandlerFuture, IntoHandlerError, IntoResponse};
 
 use hyper::{Body, Response, StatusCode};
 use futures::{future, Future, Stream};
 
-// Extract from the path
-// https://github.com/gotham-rs/gotham/blob/master/examples/path/introduction
 #[derive(Deserialize, StateData, StaticResponseExtender)]
 struct NamePathExtractor {
     name: String,
 }
 
-// Building a router
-// https://github.com/gotham-rs/gotham/tree/master/examples/routing/http_verbs
 fn router() -> Router {
     build_simple_router(|route| {
-        route.get("/hello").to(say_hello_world);
+        route.post("/hello").to(say_hello_to_body);
 
         route
-            .get("/hello/:name")
+            .get("/goodbye/:name")
             .with_path_extractor::<NamePathExtractor>()
-            .to(say_hello_to_path);
-
-        route.post("/hello").to(say_hello_to_body);
+            .to(say_goodbye_to_path_with_struct);
     })
 }
 
-fn say_hello_world(state: State) -> (State, Response) {
-    let res = create_response(
-        &state,
-        StatusCode::Ok,
-        Some((String::from("Hello World!").into_bytes(), mime::TEXT_PLAIN)),
-    );
-
-    (state, res)
+// serde attributes
+// https://serde.rs/field-attrs.html
+#[derive(Deserialize)]
+struct Who {
+    #[serde(rename = "say hello to")]
+    name: String,
 }
 
-fn say_hello_to_path(state: State) -> (State, Response) {
-    let res = {
-        let who = NamePathExtractor::borrow_from(&state);
-        create_response(
-            &state,
-            StatusCode::Ok,
-            Some((
-                format!("Hello {}!", who.name).into_bytes(),
-                mime::TEXT_PLAIN,
-            )),
-        )
-    };
-
-    (state, res)
-}
-
-// Read the body
-// https://github.com/gotham-rs/gotham/tree/master/examples/handlers/request_data
 fn say_hello_to_body(mut state: State) -> Box<HandlerFuture> {
     let f = Body::take_from(&mut state)
         .concat2()
         .then(|full_body| match full_body {
             Ok(valid_body) => {
-                let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
-                let res = create_response(
-                    &state,
-                    StatusCode::Ok,
-                    Some((
-                        format!("Hello {}!", body_content).into_bytes(),
-                        mime::TEXT_PLAIN,
-                    )),
-                );
+                let res = match serde_json::from_slice::<Who>(&valid_body.to_vec()) {
+                    Ok(who) => create_response(
+                        &state,
+                        StatusCode::Ok,
+                        Some((
+                            format!("Hello {}!", who.name).into_bytes(),
+                            mime::TEXT_PLAIN,
+                        )),
+                    ),
+                    Err(_) => create_response(&state, StatusCode::BadRequest, None),
+                };
                 future::ok((state, res))
             }
             Err(e) => return future::err((state, e.into_handler_error())),
         });
 
     Box::new(f)
+}
+
+#[derive(Serialize)]
+struct Message {
+    interjection: &'static str,
+    name: String,
+}
+
+impl IntoResponse for Message {
+    fn into_response(self, state: &State) -> Response {
+        create_response(
+            state,
+            StatusCode::Ok,
+            Some((
+                serde_json::to_string(&self)
+                    .expect("serialized Message")
+                    .into_bytes(),
+                mime::APPLICATION_JSON,
+            )),
+        )
+    }
+}
+
+fn say_goodbye_to_path_with_struct(state: State) -> (State, Message) {
+    let res = {
+        let who = NamePathExtractor::borrow_from(&state);
+        Message {
+            interjection: "Goodbye",
+            name: who.name.clone(),
+        }
+    };
+
+    (state, res)
 }
 
 pub fn main() {
